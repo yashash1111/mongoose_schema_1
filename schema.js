a444435961d96ec25bef78432b13a0977b3877ec
yashash1111 <yashash.r.s70@kalvium.community>
2025-01-28 12:09:07 +0530

    Completed the assignment

---
+++ schema.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define the User schema
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    roles: {
+        type: [String], // Array of strings
+        default: ['user']
+    },
+    profile: {
+        firstName: {
+            type: String,
+            required: false
+        },
+        lastName: {
+            type: String,
+            required: false
+        },
+        age: {
+            type: Number,
+            required: false
+        }
+    },
+    lastLogin: {
+        type: Date,
+        default: null
+    }
+}, {
+    timestamps: true 
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
